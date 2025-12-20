a31fbbe1cfa2525ee51d8d828e1755acb310d8fd
sogla <sogladev@gmail.com>
2025-12-20 16:25:28 +0100

    init
    ```sh
    bun create nuxt
    ```
    
    with content module

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-04-03',
+})
