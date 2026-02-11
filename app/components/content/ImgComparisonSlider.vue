75e7717ba63b0441b4079c5e2754d7c988d447c3
sogla <sogladev@gmail.com>
2026-02-11 17:04:49 +0100

    content(image-recolor): init
    content(icon-recolor)

---
+++ app/components/content/ImgComparisonSlider.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+
+interface Props {
+  leftImage: string
+  rightImage: string
+}
+
+const props = defineProps<Props>()
+
+const VueCompareImage = defineAsyncComponent(() =>
+  import('vue3-compare-image').then((m) => m.VueCompareImage)
+)
+</script>
+
+<template>
+  <ClientOnly>
+    <component
+      :is="VueCompareImage"
+      :left-image="leftImage"
+      :right-image="rightImage"
+    />
+  </ClientOnly>
+</template>
+
+<style scoped>
+/* Optional: Add styling for the comparison slider */
+:deep(.compare-image-container) {
+  max-width: 100%;
+  height: auto;
+}
+</style>
